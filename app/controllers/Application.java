a02bb86ab6541cef787437928d586ca68fca4b42
Kierstyn Brandt <kbrandt@ip-192-168-4-59.us-west-2.compute.internal>
2015-06-03 10:48:58 -0600

    get the tasks on JSON

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -18,4 +18,9 @@ public static Result addTask() {
         return redirect(routes.Application.index());
     }
 
+public static Result getTasks() {
+        java.util.List<models.Task> tasks = new play.db.ebean.Model.Finder(String.class, models.Task.class).all();
+        return ok(play.libs.Json.toJson(tasks));
+    }
+
 }
